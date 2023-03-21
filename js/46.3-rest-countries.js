b0d7ce78f9d974c691bdf0b70b0b3ff0a1ef1fff
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-03-22 00:51:41 +0600

    every country with flag showed

--- js/46.3-rest-countries.js
+++ js/46.3-rest-countries.js
@@ -6,14 +6,18 @@ const loadCountries = () => {
 };
 
 const displayCountries = (countries) => {
-  console.log(countries);
+  console.log(countries[0]);
   const countriesHTML = countries.map((country) => getCountryHTML(country));
+  // console.log(countriesHTML[8]);
+  const container = document.getElementById("countries");
+  container.innerHTML = countriesHTML.join(" ");
 };
 
 const getCountryHTML = (country) => {
   return `
-        <div>
-            <h2>${country.name.common}</h2>
+        <div class="country">
+            <h2>${country?.name?.common}</h2>
+            <img src="${country?.flags?.png}">
         </div>
     `;
 };
